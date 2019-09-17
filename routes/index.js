c7ff81a900c0a4c773f47d7f072c35378d4b344f
astonesh <1263293114@qq.com>
2019-09-18 00:01:09 +0800

    [one]

--- routes/index.js
+++ routes/index.js
@@ -15,5 +15,21 @@ router.get('/json', async (ctx, next) => {
     title: 'koa2 json'
   }
 })
-
+router.get('/one', async (ctx, next) => {
+  ctx.response.status = 200;
+  ctx.body = {
+    title: 'koa2 one'
+  }
+})
+router.get('/two', async (ctx, next) => {
+  ctx.response.status = 200;
+  ctx.body = {
+    title: 'koa2 one'
+  }
+  ctx.response.set('redirect11', './one/one')
+})
+router.get('/thr', async (ctx, next) => {
+  ctx.response.status = 301;
+  ctx.response.set('location', 'http://localhost:3000/one')
+})
 module.exports = router
